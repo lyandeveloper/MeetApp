fc2e44609a44e51ac8d89cd9e6a38a578091e14a
lyandeveloperm <tutors3d@gmail.com>
2020-10-28 13:01:49 -0300

    fix: peer

--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,7 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
+  path: '/peerjs',
   host: '/',
   port: '3001',
   debug: 3,
