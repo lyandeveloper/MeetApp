671b77a0bf0b495e5b9aede2c9fdfdffb4d05f1b
lyandeveloperm <tutors3d@gmail.com>
2020-10-29 16:32:31 -0300

    feat: Add error handling

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.errorHanddling();
   }
 
   middlewares() {
@@ -33,6 +34,35 @@ class App {
     this.server.use(expressLayouts);
     this.server.set('views', path.join(__dirname, 'views'));
     this.server.set('view engine', 'ejs');
+  }
+
+  errorHanddling() {
+    this.server.use((req, res, next) => {
+      res.status(404);
+
+      if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+      }
+    });
+
+    this.server.use((err, req, res, next) => {
+      console.log(err.stack);
+      return next(err);
+    });
+
+    this.server.use((err, req, res, next) => {
+      if (req.xhr) {
+        res.status(500).send({ error: '500 - Erro no server!' });
+      } else {
+        return next(err);
+      }
+    });
+
+    this.server.use((err, req, res, next) => {
+      res.status(500);
+      return res.render('error', { error: err });
+    });
   }
 
   routes() {
