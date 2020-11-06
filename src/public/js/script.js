34281c75341157ff09a74b7b9cd51fd458197a1f
lyandeveloperm <tutors3d@gmail.com>
2020-11-06 17:27:16 -0300

    feat: Change icons

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -105,32 +105,28 @@ const playStop = () => {
 
 const setMuteButton = () => {
   const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
+    <i class="fas fa-microphone"></i> 
   `;
   document.querySelector('.main__mute_button').innerHTML = html;
 };
 
 const setUnmuteButton = () => {
   const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
+    <i class="unmute fas fa-microphone-slash"  style="color: red"></i> 
   `;
   document.querySelector('.main__mute_button').innerHTML = html;
 };
 
 const setStopVideo = () => {
   const html = `
-    <i class="fas fa-video"></i>
-    <span>Stop Video</span>
+    <i class="fas fa-video"></i> 
   `;
   document.querySelector('.main__video_button').innerHTML = html;
 };
 
 const setPlayVideo = () => {
   const html = `
-  <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
+  <i class="stop fas fa-video-slash" style="color: red"></i> 
   `;
   document.querySelector('.main__video_button').innerHTML = html;
 };
